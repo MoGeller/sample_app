e66c878d57cd0eb51ecc7d7fc4f8c8b0ca7143be
Mo Geller <mgeller@optimiscorp.com>
2014-04-17 13:34:15 -0700

    Extra credit exercises.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,6 +31,13 @@ describe "UserPages" do
       end
     end
 
+    describe "after submission" do
+      before { click_button submit }
+
+      it { should have_title('Sign up') }
+      it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
